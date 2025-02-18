f094920b3dcc7a76ccbfa8a6c5c66eaa7106a099
Lapilike <lapi.like@yandex.ru>
2025-02-18 13:16:44 +0300

    Code fixes

--- src/test/java/by/lapil/audioplayer/AudioplayerApplicationTests.java
+++ src/test/java/by/lapil/audioplayer/AudioplayerApplicationTests.java
@@ -1,13 +1,1 @@
 package by.lapil.audioplayer;
-
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class AudioplayerApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
