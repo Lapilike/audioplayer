f2297ebc9948734772b05024dd0a904cd2bfad38
Lapilike <lapi.like@yandex.ru>
2025-03-04 15:02:56 +0300

    Code fixes

--- src/main/java/by/lapil/audioplayer/model/Music.java
+++ src/main/java/by/lapil/audioplayer/model/Music.java
@@ -1,15 +1,29 @@
 package by.lapil.audioplayer.model;
 
-import lombok.Builder;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "music")
 @Data
-@Builder
+@NoArgsConstructor
 public class Music {
-    private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
     private String artist;
-    private String album;
+
+    @Column(nullable = false)
     private String genre;
-    private String path;
-    private int id;
 }
