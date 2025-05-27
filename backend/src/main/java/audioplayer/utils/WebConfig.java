ff06109bcbbff193966e35b57f4d3b7e6086086d
Lapilike <lapi.like@yandex.ru>
2025-05-27 07:38:02 +0300

    Added docker

---
+++ backend/src/main/java/audioplayer/utils/WebConfig.java
@@ -0,0 +1,23 @@
+package audioplayer.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/**")  // подставь свои пути API
+                        .allowedOrigins("https://audioplayer-1.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
